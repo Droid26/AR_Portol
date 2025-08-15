7219d5cd1c0a254bb19d066f9ba305600b2eaf33
VK <vk360bm@gmail.com>
2025-08-15 12:05:25 +0530

    update the date

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-AR_Portal-0.1.0";
 const contentToCache = [
     "Build/62bacedae637cf8323226876a81ce823.loader.js",
-    "Build/3b777f44e9b93b8a55f9771a6f12ea5c.framework.js",
-    "Build/eb475262068d57439f98a6c49343c78f.data",
-    "Build/45f3925d39e744af6fa31eb4c683f9a0.wasm",
+    "Build/3b777f44e9b93b8a55f9771a6f12ea5c.framework.js.br",
+    "Build/2b363cce1946dcfd05e1fc7e21b00e97.data.br",
+    "Build/45f3925d39e744af6fa31eb4c683f9a0.wasm.br",
     "TemplateData/style.css"
 
 ];
